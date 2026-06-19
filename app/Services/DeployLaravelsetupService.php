b462f7bc8f83119e2158818ff12fb8744afb5d68
Tomáš Dulovič <tomasdulovic@MacBook-Pro-uzivatela-Tomas.local>
2026-06-19 13:16:17 +0200

    Setup ENV, KEY automate

--- app/Services/DeployLaravelsetupService.php
+++ app/Services/DeployLaravelsetupService.php
@@ -8,38 +8,60 @@ use Illuminate\Support\Str;
 use Exception;
 use Symfony\Component\Process\Process;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\File;
 
 class DeployLaravelsetupService
 {
     public function setup( string $dbname, string $dbuser, string $dbpassword, string $path)
-    {        
-        copy(
-            $path . '/.env.example',
-            $path . '/.env'
-        );
+    {       
+        $envPath = $path . '/.env';
+        $envExamplePath = $path . '/.env.example';
 
-        $env = file_get_contents($path . '/.env');
+        if (! File::exists($envPath)) {
+            File::copy($envExamplePath, $envPath);
+        }
+
+        $env = File::get($envPath);
+
 
         $env = str_replace(
-            'DB_DATABASE=laravel',
+            '# DB_HOST=127.0.0.1',
+            'DB_HOST=127.0.0.1',
+            $env
+        );
+
+        $env = str_replace(
+            '# DB_PORT=3306',
+            'DB_PORT=3306',
+            $env
+        );
+
+        $env = str_replace(
+            'DB_CONNECTION=sqlite',
+            'DB_CONNECTION=' . 'mysql',
+            $env
+        );
+
+        $env = str_replace(
+            '# DB_DATABASE=laravel',
             'DB_DATABASE=' . $dbname,
             $env
         );
 
         $env = str_replace(
-            'DB_USERNAME=root',
+            '# DB_USERNAME=root',
             'DB_USERNAME=' . $dbuser,
             $env
         );
 
         $env = str_replace(
-            'DB_PASSWORD=',
+            '# DB_PASSWORD=',
             'DB_PASSWORD=' . Crypt::decryptString($dbpassword),
             $env
         );
 
-        $env = file_put_contents(
-            $path . '/.env',
+        $env = File::put(
+            $envPath,
             $env
         );
         
